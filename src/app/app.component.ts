1b5930c01239b1f658993357de401ba6fad67673
karlwolf <karlwolf.srs@gmail.com>
2023-01-24 14:16:20 -0300

    1 file modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,12 +18,19 @@ export class AppComponent implements AfterViewInit {
   }
 
   private loadMap(): void {
+    var profileIcon = L.icon({
+      iconUrl: 'assets/images/profile-default.png',
+      iconSize: [40, 40], // size of the icon
+      iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+      popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+    });
+
     var map = L.map('map').setView([51.505, -0.09], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution:
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map);
-    var marker = L.marker([51.5, -0.09]).addTo(map);
+    var marker = L.marker([51.5, -0.09], { icon: profileIcon }).addTo(map);
   }
 }
